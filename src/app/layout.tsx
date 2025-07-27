957997e75b15f3bd111cef6960cb4db59a83b7fc
deven lacognata <dlacognata3@gmail.com>
2025-07-27 06:06:15 +0000

    WHY IN THE ACTUAL FUCK DID YOU CHANGE THE APP BACK TO FiscalFlow, ITS Ce

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
+
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 import { ThemeProvider } from '@/components/theme-provider';
 
 export const metadata: Metadata = {
-  title: 'FiscalFlow',
+  title: 'Centsei',
   description: 'Your personal finance sensei',
   manifest: '/manifest.json',
 };
