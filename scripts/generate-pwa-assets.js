93aa7018ec14083b6396e32ef13f113fc6e54b0e
deven lacognata <dlacognata3@gmail.com>
2025-08-09 14:25:24 +0000

    how can make the icon that is shown after downloading the app to be the

--- scripts/generate-pwa-assets.js
+++ scripts/generate-pwa-assets.js
@@ -7,14 +7,16 @@ const svgIcon = `
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 24 24"
     fill="none"
-    stroke="currentColor"
+    stroke="#38A3A5"
     strokeWidth="2"
     strokeLinecap="round"
     strokeLinejoin="round"
   >
-    <path d="M12 2L2 7l10 5 10-5-10-5z" />
-    <path d="M2 17l10 5 10-5" />
-    <path d="M2 12l10 5 10-5" />
+    <path d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2z" />
+    <path d="M12 18a6 6 0 0 0 6-6h-6V6a6 6 0 0 0-6 6h6v6z" />
+    <path d="M12 6V3" />
+    <path d="M12 3l2 2" />
+    <path d="M12 3l-2 2" />
   </svg>
 `;
 
