0a1a56b2f683b13c7593c9b9b229c39c94a839e1
deven lacognata <dlacognata3@gmail.com>
2025-08-12 23:10:17 +0000

    what the fuck is this supposed to be

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -3,13 +3,27 @@ import type { SVGProps } from "react";
 
 export const Logo = (props: SVGProps<SVGSVGElement>) => (
     <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="500" height="500" viewBox="0 0 500 500" {...props}>
-        <circle cx="250" cy="250" r="200" fill="#F7F4EF"/>
-        <path d="M100,380 V150 h50 v170 h150 v-170 h50 v230 h-250
-                 M125,150 h250 a20,20 0 0,0 -250,0
-                 M100,150 a150,20 0 0,1 300,0"
-              fill="#D12920"
-              stroke="#D12920"
-              strokeWidth="5"
-        />
+        <defs>
+            <filter id="shadow" x="-20%" y="-20%" width="140%" height="140%">
+                <feGaussianBlur in="SourceAlpha" stdDeviation="3"/>
+                <feOffset dx="2" dy="2" result="offsetblur"/>
+                <feComponentTransfer>
+                    <feFuncA type="linear" slope="0.5"/>
+                </feComponentTransfer>
+                <feMerge>
+                    <feMergeNode/>
+                    <feMergeNode in="SourceGraphic"/>
+                </feMerge>
+            </filter>
+        </defs>
+        <g filter="url(#shadow)">
+            <circle cx="250" cy="250" r="200" fill="#F7F4EF"/>
+            <g transform="translate(100, 150) scale(1.5)">
+                <path d="M20,150 V50 h20 v100 z" fill="#D12920" />
+                <path d="M160,150 V50 h20 v100 z" fill="#D12920" />
+                <path d="M0,50 Q100,20 200,50 L180,50 Q100,40 20,50 z" fill="#D12920" />
+                 <rect x="20" y="140" width="160" height="15" fill="#D12920" />
+            </g>
+        </g>
     </svg>
 )
